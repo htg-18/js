3392d06ab7914ec1c4e998cf70ab7a1793451830
Harsh Shah <hs553608@gmail.com>
2023-10-17 00:28:21 +0530

    more obj

---
+++ obj1.js
@@ -0,0 +1,10 @@
+const course={
+    name:"js",
+    price:"99",
+    instructor:"harsh"
+}
+//onject destructure
+
+const {name,price,instructor:ins}=course
+console.log(name)
+console.log(ins)
--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@
 // Object.create  =>constructor methodsingleton is formed
 //name age location are all treated as strigs by the system
 
+//OBJECT LITERAL
 const mySym=Symbol("key1")
 const myObj={
     name:"harsh",
@@ -27,3 +28,18 @@ myObj.greetings=function(){
    return  console.log(`Hello ${this.name}`)
 }
 console.log(myObj.greetings())
+
+//ASSIGN METHOD
+const obj1={
+    a:1,b:2
+}
+const obj2={
+    c:3,d:4,e:5
+}
+
+// stores the new object in obj4 abut also changes the obj1
+// const onj4=Object.assign(obj1,obj2)
+// const onj3=Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}
+
+console.log(obj3)
