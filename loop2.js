adf99fe6ca6318b4f7f8a2881762752761706685
Harsh Shah <hs553608@gmail.com>
2023-10-20 03:22:49 +0530

    adv loops

--- loop2.js
+++ loop2.js
@@ -18,3 +18,26 @@ const myCoding=[
 myCoding.forEach( (namee)=>{
     console.log(namee.price)
 } )
+//for each looep does not return a value even if a return statement is added to the loop
+// so we need more types of loopes
+//filter
+const arr=[1,2,3,4,5,6,7,8,9,10,11]
+const ans=arr.filter( (nums)=>nums>4 )
+// console.log(ans)
+//map
+const newArr=arr.map( (nums)=> nums+10 )
+console.log(newArr)
+
+//chaining
+const newAns=arr.map( (nums) => nums*10 )
+              .map( (nums) => nums+1 )
+              .filter( (nums)=>(nums>50) )
+// console.log(newAns)
+
+//find the sum of the element in the array. acc stores the cureent sum and cur stores the c
+// current element in the array
+const final=newAns.reduce( (acc,cur)=>(acc+cur),100 )
+console.log(final)
+
+// below metod is used to calculate the sum when in object 
+//const final=newAns.reduce( (acc,item)=>acc+item.price,0)
